2208f481b465aab982b4f685383ae4e78751ff22
anand k <kanandaiah9@gmail.com>
2026-03-03 01:52:34 +0530

    create

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+    default = "roboshop"
+}
+variable "env" {
+    default = "dev"
+}
+
+variable "sg_ids" {
+    default = ["sg-0c6de044b8ad97cca"]
+}
+
+variable "component" {
+    default = "catalogue"
+}
